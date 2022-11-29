64b85d381b63e1d3a53e7946c106da987c4b68e4
Cesar Filho <caugusto9871@gmail.com>
2022-11-29 10:07:47 -0400

    updates

--- src/db/MainDB.java
+++ src/db/MainDB.java
@@ -3,7 +3,13 @@ package db;
 import java.util.ArrayList;
 import java.util.List;
 
+import model.entities.BestSellerBook;
 import model.entities.Book;
+import model.entities.BookwormUser;
+import model.entities.GeekUser;
+import model.entities.NewlyReleasedBook;
+import model.entities.PremiumBook;
+import model.entities.PremiumUser;
 import model.entities.User;
 
 public class MainDB {
@@ -13,7 +19,8 @@ public class MainDB {
 	
 	
 	public MainDB() {
-
+		bookList = initializeBooks(bookList);
+		userList = initializeUsers(userList);
 	}
 	
 	public void addNewUser(User user) {
@@ -40,5 +47,22 @@ public class MainDB {
 		bookList.add(book);
 	}
 	
+	
+	
+	private static List<Book> initializeBooks(List<Book> list) {
+		list.add(new Book("O alienista", "Machado de Assis", "Principis", "Ficcao", 14.50, 22));
+		list.add(new BestSellerBook("O Codigo de Da Vinci", "Dan Brown", "Handom House", "Suspense", 58.50, 1));
+		list.add(new BestSellerBook("O menino grande", "Dan Brown", "Random House", "Suspense", 80.25, 23));
+		list.add(new PremiumBook("O martelo", "Joseph Mbappe", "Mullan Bruks", "Acao", 60.50, 13));
+		list.add(new NewlyReleasedBook("Um novo mundo", "Lucy", "Amazon", "Romance", 10.50, 22));
+		return list;
+	}
+	private static List<User> initializeUsers(List<User> list) {
+		list.add(new User("Leandro Pet", "Rua dubai, 7, Boa esperanca, Cuiaba-MT", "54876859784"));
+		list.add(new GeekUser("Cesar Pombo", "Rua alakasam, 22, Boa esperanca, Cuiaba-MT", "74876845784"));
+		list.add(new PremiumUser("Guilherme Seni", "Rua muamba, 13, Coxipo, Cuiaba-MT", "78876669784"));
+		list.add(new BookwormUser("Casemiro Vasco", "Rua laura, 50, Santa rosa, Cuiaba-MT", "56976859784"));
+		return list;
+	}
 	
 }
